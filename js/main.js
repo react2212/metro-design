95def4985f09ed2056c9dfea8164969bf93bf092
tubi55 <tubi55@nate.com>
2022-12-26 21:39:26 +0900

    탭박스 기능 구현

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const btnMetro = document.querySelectorAll('#metro nav a');
+const boxMetro = document.querySelectorAll('#metro section');
+
+btnMetro.forEach((btn, idx) => {
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		for (const el of btnMetro) el.classList.remove('on');
+		btnMetro[idx].classList.add('on');
+
+		for (const el of boxMetro) el.classList.remove('on');
+		boxMetro[idx].classList.add('on');
+	});
+});
